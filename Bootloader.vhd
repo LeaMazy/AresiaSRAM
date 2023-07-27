f6df357a98ffa764768ca99f29a81d189038fb99
mia-nguyen <nminhanh2000@gmail.com>
2023-07-27 10:44:42 +0200

    feat : update boot C code

--- Bootloader.vhd
+++ Bootloader.vhd
@@ -21,17 +21,19 @@ end entity;
 
 -- ARCHITECTURE
 architecture archi of Bootloader is
-	TYPE ROM IS ARRAY(0 TO 71) OF std_logic_vector(0 to 31);
+	TYPE ROM IS ARRAY(0 TO 82) OF std_logic_vector(0 to 31);
 	SIGNAL rom_block : ROM :=(
-		x"00001137" , x"00c000ef" , x"00100073" , x"0000006f" , x"80000737" , x"000107b7" , x"fff78793" , x"00f72423",
-		x"83c0c7b7" , x"08778793" , x"00f72223" , x"01800693" , x"00000313" , x"c0000737" , x"ff800613" , x"00072783",
-		x"0047f793" , x"fe078ce3" , x"00472783" , x"00072503" , x"00157513" , x"fe051ce3" , x"00f72223" , x"00d797b3",
-		x"00f36333" , x"ff868693" , x"fcc69ae3" , x"08030263" , x"fffffe37" , x"004e0e13" , x"01c30e33" , x"00030893",
-		x"c0000737" , x"ff800813" , x"00c0006f" , x"06088263" , x"071e0063" , x"41130633" , x"00050593" , x"01800693",
-		x"00072783" , x"0047f793" , x"fe078ce3" , x"00472783" , x"00d797b3" , x"00f5e5b3" , x"ff868693" , x"ff0692e3",
-		x"ffc88893" , x"00b62023" , x"01800693" , x"00d5d633" , x"0ff67613" , x"00072783" , x"0017f793" , x"fe079ce3",
-		x"00c72223" , x"ff868693" , x"ff0692e3" , x"fa1ff06f" , x"00000693" , x"c0000737" , x"00001637" , x"ffc60613",
-		x"00072783" , x"0017f793" , x"fe079ce3" , x"0006c783" , x"00f72223" , x"00168693" , x"fec694e3" , x"0000006f"
+		x"00001137" , x"00c000ef" , x"00100073" , x"0000006f" , x"ff010113" , x"00012623" , x"00012423" , x"00012223",
+		x"80000737" , x"000107b7" , x"fff78793" , x"00f72423" , x"83c0c7b7" , x"08778793" , x"00f72223" , x"01800613",
+		x"00000893" , x"c0000737" , x"ff800593" , x"00072783" , x"0047f793" , x"fe078ce3" , x"00472683" , x"00072783",
+		x"0017f793" , x"fe079ce3" , x"00d72223" , x"00c696b3" , x"00d8e8b3" , x"ff860613" , x"fcb61ae3" , x"0a088263",
+		x"ffffe337" , x"00688333" , x"00088513" , x"c0000737" , x"ff800613" , x"00c0006f" , x"08050463" , x"08a30263",
+		x"40a88833" , x"00012423" , x"01800693" , x"00072783" , x"0047f793" , x"fe078ce3" , x"00472783" , x"00f12623",
+		x"00c12783" , x"00812583" , x"00d797b3" , x"00b7e7b3" , x"00f12423" , x"ff868693" , x"fcc69ae3" , x"ffc50513",
+		x"00812783" , x"00f82023" , x"00f12423" , x"01800693" , x"00812783" , x"00d7d7b3" , x"0ff7f793" , x"00f12223",
+		x"00072783" , x"0017f793" , x"fe079ce3" , x"00412783" , x"00f72223" , x"ff868693" , x"fcc69ce3" , x"f7dff06f",
+		x"00000693" , x"c0000737" , x"00002637" , x"00072783" , x"0017f793" , x"fe079ce3" , x"0006c783" , x"00f72223",
+		x"00168693" , x"fec694e3" , x"0000006f"
 	);
 	
 	signal sigad : integer;
@@ -40,7 +42,7 @@ architecture archi of Bootloader is
 	begin
 		sigpc <= addrInstBoot(11 downto 0);
 		instBoot <= rom_block(sigad) when rising_edge(clk);
-		sigad <= 71 when ((unsigned(sigpc) > 71)) else to_integer(unsigned(sigpc));
+		sigad <= 82 when ((unsigned(sigpc) > 82)) else to_integer(unsigned(sigpc));
 	
 end archi;
 -- END FILE
